96404b4a1c7f5e36198381baf6cbb7723e853d94
Dagapa <estebangallego2002@gmail.com>
2022-12-06 17:17:02 -0500

    ecmascrit 8

---
+++ es8/03-string-padding.js
@@ -0,0 +1,4 @@
+const string = 'Hello';
+
+console.log(string.padStart(8, '_'));
+console.log(string.padEnd(8,'_'));
---
+++ es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+const array = [24, 60, 38, 20, , , 40];
+
+console.log(array);
+console.log(array.length);
